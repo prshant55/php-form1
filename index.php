30fa4679062d8fb8978e12e725de3ba20b4cd0f0
prshant55 <72465599+prshant55@users.noreply.github.com>
2020-12-08 13:33:53 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,24 @@
 <?php
-echo '<p>This is PHP!</p>';
+//echo '<p>This is PHP!</p>';
+if(isset($_GET[FirstName])){//show feedback
+echo $_GET[FirstName];
+
+
+}else{
+echo'//show form
+<form action="">
+First Name<input text="text" name="FirstName" />
+<br />
+<input type="submit">
+</form>
+';
+}
+
+
+
+
+
+
 ?>
 
-<p>This is HTML!</p>
 
